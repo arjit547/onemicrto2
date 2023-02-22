9a12e85edab710137476fcf191f43ab0781f97c6
Arjit2098 <chauhanarjit@seasiainfotech.com>
2023-02-22 17:43:40 +0530

    edw3

---
+++ service3/app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('INDIAN ARMY');
+});
+
+server.listen(80, () => {
+  console.log('Server listening on port 80');
+});
